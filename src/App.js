07cbf8f6e16a3e457d0a0c5af0c3f53406b71499
adamcardoso <adamjuniorcardoso@gmail.com>
2023-05-09 20:27:57 -0300

    Monsters Rolodex - CardList Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component} from 'react';
-
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -37,12 +37,13 @@ class App extends Component {
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
         {
-          filteredMonsters.map((monster) => {
+          /*filteredMonsters.map((monster) => {
             return <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>;
-          })
+          })*/
         }
+        <CardList/>
       </div>
     );
   }
