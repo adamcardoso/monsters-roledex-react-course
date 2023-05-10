ff01b305b099f11cc983ad9a082e2a0549ea2bd8
adamcardoso <adamjuniorcardoso@gmail.com>
2023-05-09 21:28:29 -0300

    Monsters Rolodex - CardList Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import './card-list.styles.css';
 
 class CardList extends Component {
   render() {
@@ -7,9 +8,15 @@ class CardList extends Component {
 
     return (
       <div className='card-list'>
-        {monsters.map((monster) => (
-            <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        {monsters.map((monster) => {
+          const {id, name, email} = monster;
+            return (
+            <div className='card-container' key={id}>
+              <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+        )})}
       </div>
     );
   }
